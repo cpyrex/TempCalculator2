1e2bed723f6064af852500b876a38c3d066f3b2d
cpyrex <chazknox78@gmail.com>
2016-01-26 20:02:33 -0800

    draft

--- app/src/main/java/com/knox/chaz/tempcalculator/TempCalculaterActivity.java
+++ app/src/main/java/com/knox/chaz/tempcalculator/TempCalculaterActivity.java
@@ -47,7 +47,7 @@ public class TempCalculaterActivity extends AppCompatActivity implements TextVie
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "App created by Chaz Knox", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
 
             }
